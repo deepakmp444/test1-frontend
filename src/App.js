e9860732c40139aa32344a980b67b671ecf60c4f
Deepak Kumar <48873989+deepakmp444@users.noreply.github.com>
2023-01-27 12:39:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
 import axios from "axios";
 import { useState } from "react";
-
+import { useCookies } from "react-cookie";
 function App() {
   const [message, setMessage] = useState("");
   const [getmessage, setGetMessage] = useState("");
-
+  const [cookies, setCookie] = useCookies(["name"]);
   const makeCookies = async (e) => {
     // Hint process.env.REACT_APP_API = "REACT_APP_API = "http://localhost:4000"
     e.preventDefault();
-    console.log('`${process.env.REACT_APP_API}/message`:', `${process.env.REACT_APP_API}/message`)
+
     const response = await axios.post(`${process.env.REACT_APP_API}/message`, {
       message,
     });
     console.log("response:", response.data.status);
-    localStorage.setItem("display", response.data.status)
+    localStorage.setItem("display", response.data.status);
   };
-
   const getCookies = async () => {
     try {
       const getMessageCookie = await axios.get(
         `${process.env.REACT_APP_API}/message`
       );
-       setGetMessage(getMessageCookie.data.cookieMessage);
+      setGetMessage(getMessageCookie.data.cookieMessage);
       console.log("getMessageCookie:", getMessageCookie.data.cookieMessage);
     } catch (error) {
       console.log("error:", error);
     }
+  };
+
+  const handleClearCookie = async () => {
+    try {
+      const getMessageCookie = await axios.get(
+        `${process.env.REACT_APP_API}/clear`
+      );
+
+      console.log("getMessageCookie:", getMessageCookie.data);
+    } catch (error) {}
   };
 
   return (
@@ -42,11 +51,15 @@ function App() {
         </button>
       </form>
 
+      <button type="button" onClick={handleClearCookie}>
+        Clear Cookies
+      </button>
+
       <button type="button" onClick={getCookies}>
         Get Cookies
       </button>
- <br />
-{getmessage === "undefined" ? "undefined Value" : getmessage}
+      <br />
+      {getmessage === "undefined" ? <h1>undefined</h1> : getmessage}
     </div>
   );
 }
